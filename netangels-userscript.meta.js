7972d9aad60ac6ad17cf3c4d9278cdfb2da6f8bc
crashmax-dev <crashmax.dev@gmail.com>
2022-05-28 12:51:41 +0000

    Deploying to gh-pages from @ crashmax-dev/netangels-userscript@64574c38eb84541d48402381a8136c1404568e27 🚀

---
+++ netangels-userscript.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        netangels-userscript
+// @version     1.0.0
+// @author      Vitalij Ryndin
+// @description netangels-userscript
+// @homepage    https://crashmax-dev.github.io/netangels-userscript/
+// @updateURL   https://crashmax-dev.github.io/netangels-userscript/netangels-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/netangels-userscript/netangels-userscript.user.js
+// @include     /^(http|https)://(panel.netangels.ru).*$/
+// ==/UserScript==
